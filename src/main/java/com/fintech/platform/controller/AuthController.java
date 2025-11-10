ce82a38902f609dcee4d6b7d98fc60fe008a6c22
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-10 23:34:24 +0700

    Create CController for Auth register

---
+++ src/main/java/com/fintech/platform/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.fintech.platform.controller;
+
+import com.fintech.platform.dto.RegisterRequest;
+import com.fintech.platform.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> Register(@RequestBody RegisterRequest request) {
+        String resp = authService.register(request);
+        return ResponseEntity.ok(resp);
+    }
+}
