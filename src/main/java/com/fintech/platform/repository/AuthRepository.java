5edf13c5b2c31c261b669dfcb793801cf98a0644
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-10 23:33:18 +0700

    Create Repository for Auth and extends by JPA

---
+++ src/main/java/com/fintech/platform/repository/AuthRepository.java
@@ -0,0 +1,8 @@
+package com.fintech.platform.repository;
+
+import com.fintech.platform.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+}
