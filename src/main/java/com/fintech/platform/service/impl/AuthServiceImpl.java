f46adee83f3475fab387ecc4fc1e06272c26114c
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-11 20:53:46 +0700

    Add PasswordEncoder Bean

--- src/main/java/com/fintech/platform/service/impl/AuthServiceImpl.java
+++ src/main/java/com/fintech/platform/service/impl/AuthServiceImpl.java
@@ -5,19 +5,24 @@ import com.fintech.platform.entity.User;
 import com.fintech.platform.repository.AuthRepository;
 import com.fintech.platform.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
     @Autowired
     private AuthRepository authRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public String register(RegisterRequest request){
         if(authRepository.existsByEmail(request.getEmail())) {
             return "email has already registered";
         }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
         User user = new User();
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
+        user.setPassword(encodedPassword);
         authRepository.save(user);
         return "email is registered";
     }
