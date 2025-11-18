64d6aece63b1ec6588a63b6559cfac4ae4fd6fb7
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-18 22:45:43 +0700

    Add security AppUserDetailsService.java

--- src/main/java/com/fintech/platform/repository/UserRepository.java
+++ src/main/java/com/fintech/platform/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.fintech.platform.repository;
 import com.fintech.platform.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
