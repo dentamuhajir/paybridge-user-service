b1a5be4b091e39df6188507785b6eb507879e4ac
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-19 19:35:23 +0700

    Grouping endpoint with RequestMapping Annotation

--- src/main/java/com/fintech/platform/controller/AuthController.java
+++ src/main/java/com/fintech/platform/controller/AuthController.java
@@ -8,12 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     @Autowired
