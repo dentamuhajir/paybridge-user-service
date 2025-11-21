e69a5177e5d0ca9f71fc344b25f9b5da829b57a8
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-21 21:02:47 +0700

    Add jwtAuthenticationFilter for validating token to authenticated endpoints

--- src/main/java/com/fintech/platform/config/SecurityConfig.java
+++ src/main/java/com/fintech/platform/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.fintech.platform.config;
 
+import com.fintech.platform.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -23,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/health").permitAll()
                         .requestMatchers("/users/**").authenticated()
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(form -> form.disable()) // disable default login form
                 .httpBasic(basic -> basic.disable()); // disable basic auth popup
         return http.build();
