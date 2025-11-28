2bd7942f47b09f1ac4633122e617b603cd11b930
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-28 19:16:55 +0700

    Add validation blank

--- src/main/java/com/paybridge/user/service/dto/RegisterRequest.java
+++ src/main/java/com/paybridge/user/service/dto/RegisterRequest.java
@@ -1,12 +1,17 @@
 package com.paybridge.user.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    @NotBlank
     private String fullName;
+    @NotBlank(message = "is required")
     private String email;
+    @NotBlank(message = "Password is required")
     private String password;
+    @NotBlank(message = "is required")
     private String phoneNumber;
 
 
