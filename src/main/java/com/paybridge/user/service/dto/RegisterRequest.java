7d4fdd8f7e540a4ae016e4a2c6855e583dcce55d
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-11-28 19:17:58 +0700

    Add validation Email & Size

--- src/main/java/com/paybridge/user/service/dto/RegisterRequest.java
+++ src/main/java/com/paybridge/user/service/dto/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.paybridge.user.service.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -8,8 +10,10 @@ public class RegisterRequest {
     @NotBlank
     private String fullName;
     @NotBlank(message = "is required")
+    @Email(message = "Invalid email format")
     private String email;
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
     @NotBlank(message = "is required")
     private String phoneNumber;
