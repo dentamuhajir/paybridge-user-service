9fd3d4bc2baa102de16f223bc22bc8f1cda4a85e
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-12-02 22:17:30 +0700

    Update DTO field to snake case

--- src/main/java/com/paybridge/user/service/dto/WalletCreateRequest.java
+++ src/main/java/com/paybridge/user/service/dto/WalletCreateRequest.java
@@ -1,10 +1,12 @@
 package com.paybridge.user.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class WalletCreateRequest {
+    @JsonProperty("user_id")
     @NotBlank
     private String userId;
     @NotBlank
